8e6b5d933ef6d45b2aa8a98482af86ed310914fc
Mehmethan Yüksel <mehmethanyuksel@galatasaray.net>
2016-12-03 14:11:31 +0200

    Add files via upload

--- sudoku_Projesi/Form1.cs
+++ sudoku_Projesi/Form1.cs
@@ -19,53 +19,75 @@ namespace sudoku_Projesi
             frm = this;
         }
         public int[,] matris = new int[9, 9];
+        Boolean kontroleKontrol = false;
         private void Form1_Load(object sender, EventArgs e)
         {
            AddBox addtxt = new AddBox();
-
-           
         }
         private void matriseAl()
         {
-            for (int i = 1; i <= 9; i++)
-                for (int j = 1; j <= 9; j++)
-                    matris[i - 1, j - 1] = int.Parse(((TextBox)this.Controls["textbox" + i + j]).Text); // textboxtaki değerleri matrise atadık
+            try
+            {
+                for (int i = 1; i <= 9; i++)
+                    for (int j = 1; j <= 9; j++)
+                        matris[i - 1, j - 1] = int.Parse(((TextBox)this.Controls["textbox" + i + j]).Text); // textboxtaki değerleri matrise atadık
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lütfen tüm alanları doldurup yeniden deneyiniz.");
+                kontroleKontrol = true;
+            }
         }
         private void button1_Click(object sender, EventArgs e)
         {
             matriseAl();
-            int tut=0;
-            Boolean kontrol = false;
-            try
+            if (kontroleKontrol==false)
             {
-                if (kontrol == true)
+                kontrolEt kontroll = new kontrolEt(matris);
+                MessageBox.Show(kontroll.text);
+            }
+            else
+                kontroleKontrol = false;
+            
+           /* int tut=0,tut2=0,tut3=0;
+            Boolean kontrol = false,kontrol2=false,kontrol3=false;
+            
+                if (kontrol == false)
                     for (int i = 0; i < 9; i++)
                     {
-                        if (kontrol == true)
+                        if (kontrol == false)
                             for (int j = 0; j < 9; j++)
                             {
                                 tut = matris[i, j];
                                 if (kontrol == false) 
                                     for (int k = j+1; k < 9; k++)
-                                    {
                                         if (tut == matris[i, k])
                                         {
                                             kontrol = true;
-                                            //MessageBox.Show(i+". satır yanlış");
-                                            frm.Controls["textbox" + i + j].BackColor = System.Drawing.SystemColors.ScrollBar;
-                               
-
+                                            MessageBox.Show((i+1)+". satırda hata var");
+                                            break;
+                                           // frm.Controls["textbox" + i + j].BackColor = System.Drawing.SystemColors.ScrollBar;//textbox renklendirme işlemini hallet
                                         }
-                                    }
+                                tut2 = matris[j, i];
+                                if (kontrol2 == false)
+                                    for (int k = j + 1; k < 9; k++)
+                                        if (tut == matris[k, i])
+                                        {
+                                            kontrol2 = true;
+                                            MessageBox.Show((i + 1) + ". sütunda hata var");
+                                            break;
+                                        }
                             }
                     }
+                
+                
+                            }
                 kontrol = false;
-              
-            }
-            catch(Exception)
-            {
-                MessageBox.Show("Lütfen tüm alanları doldurup tekrar deneyiniz.");
-            }
+                kontrol2 = false;
+                kontrol3 = false;
+            */
+          
             
             
         }
@@ -82,7 +104,7 @@ namespace sudoku_Projesi
         private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
-        }//Kontrol etme olayı
+        }
 
         
        
--- sudoku_Projesi/kontrolEt.cs
+++ sudoku_Projesi/kontrolEt.cs
@@ -6,7 +6,75 @@ using System.Threading.Tasks;
 
 namespace sudoku_Projesi
 {
-    class kontrolEt
+    class kontrolEt : Form1
     {
+        public string text;
+        public Boolean kontrol = false;
+        private int tuty = 0, tutd = 0, tut;
+        Form1 fm1;
+        private int[,] matrisim = new int[9, 9];
+        public kontrolEt(int[,] matris)
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    matrisim[i, j] = matris[i, j];
+
+
+            satirsutun();
+            dokuzluKontrol();
+        }
+
+        private void satirsutun()
+        {
+
+            if (kontrol == false)
+                for (int i = 0; i < 9; i++)
+                {
+                    if (kontrol == false)
+                        for (int j = 0; j < 9; j++)
+                        {
+                            tuty = matrisim[i, j];
+                            tutd = matrisim[j, i];
+                            if (kontrol == false)
+                                for (int k = j + 1; k < 9; k++)
+                                    if (tuty == matrisim[i, k])
+                                    {
+                                        kontrol = true;
+                                        text = (i + 1) + ". Satırda hata tespit edildi";
+                                        break;
+                                        // frm.Controls["textbox" + i + j].BackColor = System.Drawing.SystemColors.ScrollBar;//textbox renklendirme işlemini hallet
+                                    }
+                            if (kontrol == false)
+                                for (int k = j + 1; k < 9; k++)
+                                    if (tutd == matrisim[k, i])
+                                    {
+                                        kontrol = true;
+                                        text = (i + 1) + ". Sütunda hata tespit edildi";
+                                        break;
+                                    }
+                        }
+                }
+        }
+
+        private void dokuzluKontrol()
+        {
+            if (kontrol == false)
+                for (int i = 0; i < 3; i++)
+                    if (kontrol == false)
+                        for (int j = 0; j < 3; j++)
+                        {
+                            tut = matris[i, j];
+                            if (kontrol == false)
+                                for (int k = j; k < 3; k++)
+                                    if (kontrol == false)
+                                        for (int l = 0; l < 3; l++)
+                                            if (k != 0 || l != 0)
+                                                if (tut == matris[k, l])
+                                                {
+                                                    kontrol = true;
+                                                    text = "9'lu içerisinde hata tespit edildi";
+                                                }
+                        }
+        }
     }
 }
