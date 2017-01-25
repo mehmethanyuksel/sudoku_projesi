00dee6388da3f74a8ea05d79d13a10aa0c4e19ac
Mehmethan Yüksel <mehmethanyuksel@galatasaray.net>
2017-01-25 18:10:00 +0200

    Add files via upload

--- sudoku_Projesi/Form1.Designer.cs
+++ sudoku_Projesi/Form1.Designer.cs
@@ -33,20 +33,32 @@
             this.linkLabel4 = new System.Windows.Forms.LinkLabel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.kadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.skorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kistatistikBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.db_sudokuDataSet = new sudoku_Projesi.db_sudokuDataSet();
             this.kistatistikTableAdapter = new sudoku_Projesi.db_sudokuDataSetTableAdapters.kistatistikTableAdapter();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.kistatistikBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.db_sudokuDataSet)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -56,7 +68,7 @@
             // linkLabel4
             // 
             this.linkLabel4.AutoSize = true;
-            this.linkLabel4.Location = new System.Drawing.Point(36, 28);
+            this.linkLabel4.Location = new System.Drawing.Point(9, 24);
             this.linkLabel4.Name = "linkLabel4";
             this.linkLabel4.Size = new System.Drawing.Size(49, 13);
             this.linkLabel4.TabIndex = 8;
@@ -76,40 +88,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(34, 12);
+            this.label2.Location = new System.Drawing.Point(9, 6);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 13);
             this.label2.TabIndex = 10;
             this.label2.Text = "Lütfen giriş yapınız";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.kadiDataGridViewTextBoxColumn,
-            this.skorDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.kistatistikBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 251);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(205, 113);
-            this.dataGridView1.TabIndex = 13;
-            this.dataGridView1.ColumnAdded += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnAdded);
-            // 
-            // kadiDataGridViewTextBoxColumn
-            // 
-            this.kadiDataGridViewTextBoxColumn.DataPropertyName = "k_adi";
-            this.kadiDataGridViewTextBoxColumn.HeaderText = "k_adi";
-            this.kadiDataGridViewTextBoxColumn.Name = "kadiDataGridViewTextBoxColumn";
-            this.kadiDataGridViewTextBoxColumn.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
-            this.kadiDataGridViewTextBoxColumn.Width = 70;
-            // 
-            // skorDataGridViewTextBoxColumn
-            // 
-            this.skorDataGridViewTextBoxColumn.DataPropertyName = "skor";
-            this.skorDataGridViewTextBoxColumn.HeaderText = "skor";
-            this.skorDataGridViewTextBoxColumn.Name = "skorDataGridViewTextBoxColumn";
-            this.skorDataGridViewTextBoxColumn.Width = 70;
             // 
             // kistatistikBindingSource
             // 
@@ -125,6 +108,150 @@
             // 
             this.kistatistikTableAdapter.ClearBeforeFill = true;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.AutoSize = false;
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.LightCyan;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4,
+            this.toolStripMenuItem5,
+            this.toolStripMenuItem6,
+            this.toolStripMenuItem7,
+            this.toolStripMenuItem8,
+            this.toolStripMenuItem9});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(180, 185);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.AutoSize = false;
+            this.toolStripMenuItem1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolStripMenuItem1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuItem1.Margin = new System.Windows.Forms.Padding(10, 10, 0, 0);
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem1.Text = "1    ";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.AutoSize = false;
+            this.toolStripMenuItem2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripMenuItem2.Margin = new System.Windows.Forms.Padding(60, -50, 0, 0);
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem2.Text = "2";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.AutoSize = false;
+            this.toolStripMenuItem3.Margin = new System.Windows.Forms.Padding(110, -50, 0, 0);
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem3.Text = "3";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.AutoSize = false;
+            this.toolStripMenuItem4.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem4.Text = "4";
+            this.toolStripMenuItem4.Click += new System.EventHandler(this.toolStripMenuItem4_Click);
+            // 
+            // toolStripMenuItem5
+            // 
+            this.toolStripMenuItem5.AutoSize = false;
+            this.toolStripMenuItem5.Margin = new System.Windows.Forms.Padding(60, -50, 0, 0);
+            this.toolStripMenuItem5.Name = "toolStripMenuItem5";
+            this.toolStripMenuItem5.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem5.Text = "5";
+            this.toolStripMenuItem5.Click += new System.EventHandler(this.toolStripMenuItem5_Click);
+            // 
+            // toolStripMenuItem6
+            // 
+            this.toolStripMenuItem6.AutoSize = false;
+            this.toolStripMenuItem6.Margin = new System.Windows.Forms.Padding(110, -50, 0, 0);
+            this.toolStripMenuItem6.Name = "toolStripMenuItem6";
+            this.toolStripMenuItem6.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem6.Text = "6";
+            this.toolStripMenuItem6.Click += new System.EventHandler(this.toolStripMenuItem6_Click);
+            // 
+            // toolStripMenuItem7
+            // 
+            this.toolStripMenuItem7.AutoSize = false;
+            this.toolStripMenuItem7.Margin = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.toolStripMenuItem7.Name = "toolStripMenuItem7";
+            this.toolStripMenuItem7.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem7.Text = "7";
+            this.toolStripMenuItem7.Click += new System.EventHandler(this.toolStripMenuItem7_Click);
+            // 
+            // toolStripMenuItem8
+            // 
+            this.toolStripMenuItem8.AutoSize = false;
+            this.toolStripMenuItem8.Margin = new System.Windows.Forms.Padding(60, -50, 0, 0);
+            this.toolStripMenuItem8.Name = "toolStripMenuItem8";
+            this.toolStripMenuItem8.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem8.Text = "8";
+            this.toolStripMenuItem8.Click += new System.EventHandler(this.toolStripMenuItem8_Click);
+            // 
+            // toolStripMenuItem9
+            // 
+            this.toolStripMenuItem9.AutoSize = false;
+            this.toolStripMenuItem9.Margin = new System.Windows.Forms.Padding(110, -50, 0, 0);
+            this.toolStripMenuItem9.Name = "toolStripMenuItem9";
+            this.toolStripMenuItem9.Size = new System.Drawing.Size(50, 50);
+            this.toolStripMenuItem9.Text = "9";
+            this.toolStripMenuItem9.Click += new System.EventHandler(this.toolStripMenuItem9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(868, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 16;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(158, 72);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kontrol Seçenekleri";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(7, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(135, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Her dakika başı kontrol";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Buton ile kontrol";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // button5
             // 
             this.button5.BackgroundImage = global::sudoku_Projesi.Properties.Resources.dark_blue_home_icon_500x500;
@@ -139,26 +266,28 @@
             // 
             // button4
             // 
-            this.button4.BackgroundImage = global::sudoku_Projesi.Properties.Resources.karistir_butonu;
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.BackgroundImage = global::sudoku_Projesi.Properties.Resources.karistir_butonu1;
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Location = new System.Drawing.Point(12, 441);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(205, 67);
             this.button4.TabIndex = 14;
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // button3
             // 
-            this.button3.BackgroundImage = global::sudoku_Projesi.Properties.Resources.en_iyiler;
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.BackgroundImage = global::sudoku_Projesi.Properties.Resources.en_iyiler1;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Location = new System.Drawing.Point(12, 370);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(205, 65);
             this.button3.TabIndex = 12;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // button2
@@ -175,7 +304,8 @@
             // 
             // button1
             // 
-            this.button1.BackgroundImage = global::sudoku_Projesi.Properties.Resources.kontrolEttt;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImage = global::sudoku_Projesi.Properties.Resources.kontrolEttt1;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
@@ -183,7 +313,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 251);
             this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
@@ -192,22 +322,28 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
             this.ClientSize = new System.Drawing.Size(1030, 531);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel4);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sudoku";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.kistatistikBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.db_sudokuDataSet)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,14 +358,25 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private db_sudokuDataSet db_sudokuDataSet;
         private System.Windows.Forms.BindingSource kistatistikBindingSource;
         private db_sudokuDataSetTableAdapters.kistatistikTableAdapter kistatistikTableAdapter;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn kadiDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn skorDataGridViewTextBoxColumn;
         private System.Windows.Forms.Button button5;
+        public  System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
 
 
 
--- sudoku_Projesi/Form1.cs
+++ sudoku_Projesi/Form1.cs
@@ -14,7 +14,7 @@ namespace sudoku_Projesi
     {
         public static Form1 frm;
         kontrolEt kontroll;
-        private int saniye = 0, dk = 0, saat = 0;
+        private int saniye = 0, dk = 0, saat = 0,tutt=0,skor=0;
         public static Boolean dogrumu = true;
         sqlBaglanti b = new sqlBaglanti();
         public Form1()
@@ -26,42 +26,48 @@ namespace sudoku_Projesi
         Boolean kontroleKontrol = false;
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'db_sudokuDataSet.kistatistik' table. You can move, or remove it, as needed.
-            this.kistatistikTableAdapter.Fill(this.db_sudokuDataSet.kistatistik);
-            this.dataGridView1.Sort(this.dataGridView1.Columns[1], ListSortDirection.Ascending);
-            b.sc.Open();            
+            b.sc.Open();          
             baslangicDegerleri baslangic = new baslangicDegerleri();
             kullanici();
             label1.Text = "Geçen süre: " + saat.ToString() + ":" + dk.ToString() + ":" + saniye.ToString();
             timer1.Interval = 1000;
             timer1.Start();
         }
+        
         public void kullanici()
         {
             label2.Text = uye_girisi.k_adi;
-            if (uye_girisi.k_adi == "")
-            {
-                linkLabel4.Text = "Lütfen giriş yapınız!";
-            }
+            if (label2.Text == "")
+                linkLabel4.Text = "Giriş Yap";
+            else
+                linkLabel4.Text = "Çıkış Yap";
         }
-        private void matriseAl()
+        private void matriseAl(int a)
         {
             try
             {
                 for (int i = 1; i <= 9; i++)
                     for (int j = 1; j <= 9; j++)
-                        matris[i - 1, j - 1] = int.Parse(((TextBox)this.Controls["textbox" + i + j]).Text);
+                        if (a != 1)
+                        {
+                            if (Convert.ToString(this.Controls["textbox" + i + j].Text) != "")
+                                matris[i - 1, j - 1] = int.Parse(((TextBox)this.Controls["textbox" + i + j]).Text);
+                        }
+                        else
+                            matris[i - 1, j - 1] = int.Parse(((TextBox)this.Controls["textbox" + i + j]).Text);
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Lütfen tüm alanları doldurup yeniden deneyiniz.");
-                kontroleKontrol = true;
+                   if (a == 1)
+                        MessageBox.Show("Lütfen tüm alanları doldurup yeniden deneyiniz;\n"+ex.Message);
+                    else
+                        label3.Text = "Lütfen Tüm alanları doldurun!";
+                    kontroleKontrol = true;
             }
-
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            matriseAl();
+            matriseAl(1);
             if (kontroleKontrol == false)
             {
                 kontroll = new kontrolEt(matris);
@@ -69,12 +75,16 @@ namespace sudoku_Projesi
             }
             else
                 kontroleKontrol = false;
-            if (dogrumu == false && label2.Text != "")
+            if (dogrumu == false)
             {
                 MessageBox.Show("Tebrikler, sonuç doğru!");
-                b.komut.CommandText = "INSERT INTO kistatistik(k_adi,skor) VALUES ('" + Convert.ToString(uye_girisi.k_adi) + "','" + saat.ToString() + ":" + dk.ToString() + ":" + saniye.ToString() + "')";
-                b.dr = b.komut.ExecuteReader();
-                b.dr.Read();
+                if (label2.Text != "")
+                { 
+                    b.komut.CommandText = "INSERT INTO kistatistik(k_adi,skor) VALUES ('" + Convert.ToString(uye_girisi.k_adi) + "','" +skor + "')";
+                    b.dr = b.komut.ExecuteReader();
+                    b.dr.Read();
+                }
+                timer1.Stop();
             }
             else
                 dogrumu = true;
@@ -100,12 +110,28 @@ namespace sudoku_Projesi
 
         private void timer1_Tick_3(object sender, EventArgs e)
         {
+            if (checkBox1.Checked == false)
+                button1.Visible = false;
+            else
+                button1.Visible = true;
             saniye++;
+            skor++;
+            if (checkBox2.Checked != true)
+                label3.Visible = false;
+            else
+                label3.Visible = true;
             label1.Text = "SÜRE: " + saat.ToString() + ":" + dk.ToString() + ":" + saniye.ToString();
             if (saniye == 60)
             {
                 saniye = 0;
                 dk++;
+                if (checkBox2.Checked == true)
+                { 
+                    matriseAl(2);
+                    kontroll = new kontrolEt(matris);
+                    label3.Text = kontroll.text;
+                }
+                
             }
             if (dk == 60)
             {
@@ -119,7 +145,7 @@ namespace sudoku_Projesi
         {
             if (label2.Text != "")
             {
-                linkLabel4.Text = "Lütfen giriş yapınız!";
+                linkLabel4.Text = "Giriş Yap";
                 label2.Text = "";
                 uye_girisi.k_adi = "";
                 MessageBox.Show("Başarıyla çıkış yaptınız");
@@ -131,7 +157,6 @@ namespace sudoku_Projesi
                 uye_ekrani.groupBox1.BringToFront();
                 uye_ekrani.groupBox2.SendToBack();
                 kullanici();
-                linkLabel4.Text = "Çıkış Yap";
             }
         }
 
@@ -139,7 +164,6 @@ namespace sudoku_Projesi
         {
             this.Close();
         }
-
         private void button4_Click_1(object sender, EventArgs e)
         {
             baslangicDegerleri baslan = new baslangicDegerleri();
@@ -161,6 +185,52 @@ namespace sudoku_Projesi
         {
 
         }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "1";
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "2";
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "3";
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "4";
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "5";
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "6";
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "7";
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "8";
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+            this.Controls[AddBox.names + ""].Text = "9";
+        }
+      
 
     }
 
